3cc83f987199b76c703122c13de513851751b2e8
hda4508 <hda4508@gmail.com>
2025-12-07 16:23:42 +0900

    update: react portfolio

--- src/components/CursorSmoke.jsx
+++ src/components/CursorSmoke.jsx
@@ -9,23 +9,20 @@ export default function CursorSmoke() {
     if (!container) return;
 
     const handleMove = (e) => {
-      // 작은 점 하나 만들기
       const circle = document.createElement("div");
       circle.className = "circle";
 
       const x = e.clientX;
       const y = e.clientY;
 
-      // 마우스 위치 기준으로 중앙 맞추기
       circle.style.left = `${x - 5}px`;
       circle.style.top = `${y - 5}px`;
 
       container.appendChild(circle);
 
-      // 애니메이션 끝나면 DOM에서 제거
       setTimeout(() => {
         circle.remove();
-      }, 1000); // animation: 1s 이니까 1초 후 제거
+      }, 1000);
     };
 
     window.addEventListener("mousemove", handleMove);
